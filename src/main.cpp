35305476f6c64749262371679bdcae3053bdc9b9
Anders Alavik <anders@alavik.se>
2023-08-06 07:56:19 +0200

    cleanup and optimized the code.

--- src/main.cpp
+++ src/main.cpp
@@ -7,137 +7,102 @@
 #define GUP 9
 #define GDOWN 10
 
-// Enum for the curve type
 enum curveType { LINEAR, EXPONENTIAL, LOGARITHMIC };
 
-// Setup HX711 scale object
 HX711 handbrakeScale;
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID + 7, JOYSTICK_TYPE_MULTI_AXIS, 2, 0, false, false, true, false, false, false, false, true, false, true, false);
 
-// Struct to hold the settings
 struct Settings {
   curveType handbrakeCurve;
   float minRawHandbrake;
   float maxRawHandbrake;
-  float curveFactor;  // Additional parameter to adjust curve behavior
+  float curveFactor;
 };
 
-// Define default settings
 Settings defaultSettings = { EXPONENTIAL, 10000.00, 2095588, 2.0 };
-
-// Variable to hold current settings
 Settings currentSettings;
-
-// Joystick object
-Joystick_ myJoystick(JOYSTICK_DEFAULT_REPORT_ID + 7, JOYSTICK_TYPE_MULTI_AXIS, 2, 0, false, false, true, false, false, false, false, true, false, true, false);
-
-bool setupMode = false; // Added setup mode flag
+bool setupMode = false;
+int lastGUpState = 0;
+int lastGDownState = 0;
 
 void setup() {
-  // Load settings from EEPROM
   EEPROM.get(0, currentSettings);
-
-  // Check if settings are valid
   if (currentSettings.handbrakeCurve < LINEAR || currentSettings.handbrakeCurve > LOGARITHMIC || currentSettings.minRawHandbrake < -9000000 || currentSettings.maxRawHandbrake > 90000000 || currentSettings.curveFactor <= 0) {
-    // If not valid, use default settings
     currentSettings = defaultSettings;
   }
-
-  // Setup LED
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);  // Indicate start of setup
+  digitalWrite(LED_BUILTIN, HIGH); 
   pinMode(GUP, INPUT_PULLUP);
   pinMode(GDOWN, INPUT_PULLUP);
-  // Initialize HX711
   handbrakeScale.begin(DOUT, CLK);
   handbrakeScale.set_scale();
-  handbrakeScale.tare();  // Tare the scale
-
-  // Initialize joystick
-  myJoystick.setZAxisRange(0, 1023);  // Set brake range
-  myJoystick.setButton(0,0);
-  myJoystick.setButton(1,0);
-  myJoystick.begin(true);  // Start joystick in auto send mode
-
-
-  int lastGUpState = 0;
-  int lastGDownState = 0;
-
-
-
-
-  digitalWrite(LED_BUILTIN, LOW);  // Indicate end of setup
-  
-
-
-
-  // Initialize Serial
+  handbrakeScale.tare();  
+  Joystick.setZAxisRange(0, 1023);  
+  Joystick.setButton(0,0);
+  Joystick.setButton(1,0);
+  Joystick.begin(true); 
+  digitalWrite(LED_BUILTIN, LOW);  
   Serial.begin(9600);
 }
 
 void applyCurve(float& val, curveType curve, float curveFactor) {
-  // Shift the range of val to a positive domain
   val = val - currentSettings.minRawHandbrake;
-
-  // Make sure val is in the range 0-700
   val = constrain(val, 0, currentSettings.maxRawHandbrake - currentSettings.minRawHandbrake);
-  
-  // Apply curve depending on the curve type
   switch(curve) {
     case LINEAR:
-      val = val / (currentSettings.maxRawHandbrake - currentSettings.minRawHandbrake);  // Linear curve
+      val /= (currentSettings.maxRawHandbrake - currentSettings.minRawHandbrake);
       break;
     case EXPONENTIAL:
-      val = pow(val / (currentSettings.maxRawHandbrake - currentSettings.minRawHandbrake), curveFactor);  // Exponential curve with custom exponent
+      val = pow(val / (currentSettings.maxRawHandbrake - currentSettings.minRawHandbrake), curveFactor);
       break;
     case LOGARITHMIC:
-      // Logarithmic curve with custom base (check to avoid log of 0)
-      if (val > 0) // Now val is guaranteed to be non-negative, we only need to check if it's greater than 0
+      if (val > 0) 
         val = log(val / (currentSettings.maxRawHandbrake - currentSettings.minRawHandbrake)) / log(curveFactor);
       else
         val = 0;
       break;
   }
-
-  // Apply transformation to joystick brake range (0 to 1023)
-  val = val * 1023;
+  val *= 1023;
 }
 
-
+void handleButton(int& lastState, int pin, int button){
+  int currentState = !digitalRead(pin);
+  if (currentState != lastState){
+    Joystick.setButton(button, currentState);
+    lastState = currentState;
+  }
+}
 
 void loop() {
-  // Check for incoming messages
-  if (Serial.available() > 0) {
-    // Read the command
+  while (Serial.available() > 0) {
     char command = Serial.read();
-
-    // Handle the command
     switch(command) {
-      case 'c':  // Change curve type
+      case 'c':
         if (Serial.available() > 0) {
           curveType newCurve = (curveType)Serial.parseInt();
           if (newCurve >= LINEAR && newCurve <= LOGARITHMIC) {
             currentSettings.handbrakeCurve = newCurve;
           }
         }
+        break;
+      case 'm':
+          if (Serial.available() >= static_cast<int>(sizeof(float))) {
+            float newMin = Serial.parseFloat();
+            if (newMin >= -900000) {
+              currentSettings.minRawHandbrake = newMin;
+            }
+          }
+          break;
+      case 't':
+          if (Serial.available() >= static_cast<int>(sizeof(float))) {
+            float newMax = Serial.parseFloat();
+            if (newMax <= 9000000) {
+              currentSettings.maxRawHandbrake = newMax;
+            }
+          }
+          break;
 
-        break;
-      case 'm':  // Change minRawHandbrake
-        if (Serial.available() >= sizeof(float)) {
-          float newMin = Serial.parseFloat();
-          if (newMin >= -900000) {
-            currentSettings.minRawHandbrake = newMin;
-          }
-        }
-        break;
-      case 't':  // Change maxRawHandbrake
-        if (Serial.available() >= sizeof(float)) {
-          float newMax = Serial.parseFloat();
-          if (newMax <= 9000000) {
-            currentSettings.maxRawHandbrake = newMax;
-          }
-        }
-        break;
-      case 'f':  // Change curveFactor
+      case 'f':
         if (Serial.available() > 0) {
           int newFactor = Serial.parseInt();
           if (newFactor >= 0) {
@@ -145,17 +110,16 @@ void loop() {
           }
         }
         break;
-
-      case 's':  // Save settings
+      case 's':
         EEPROM.put(0, currentSettings);
         break;
-      case 'e':  // Toggle setup mode
+      case 'e':
         setupMode = true;
         break;
-      case 'w':  // Toggle setup mode
+      case 'w':
         setupMode = false;
         break;
-      case 'r':  // Read current settings
+      case 'r':
         Serial.print("Curve type: ");
         switch(currentSettings.handbrakeCurve) {
           case LINEAR:
@@ -176,45 +140,20 @@ void loop() {
         Serial.println(currentSettings.curveFactor*10);
         Serial.print(currentSettings.handbrakeCurve);
         break;
-      
     }
   }
 
-  // Read handbrake value
   float rawHandbrake = handbrakeScale.get_units();
-  float rawHandbrakeOriginal = rawHandbrake;  // Store the original raw value
+  float processedHandbrake = rawHandbrake;  // Save the original value
+  applyCurve(processedHandbrake, currentSettings.handbrakeCurve, currentSettings.curveFactor);
+  Joystick.setZAxis(processedHandbrake);
+  handleButton(lastGUpState, GUP, 0);
+  handleButton(lastGDownState, GDOWN, 1);
 
-  // Apply curve to the raw reading
-  applyCurve(rawHandbrake, currentSettings.handbrakeCurve, currentSettings.curveFactor);
-
-  // Set brake value on joystick
-  myJoystick.setZAxis(rawHandbrake);
-
-  int lastGUpState = 0;
-  int lastGDownState = 0;
-
-  // Gear controll
-  int currentGUpState = !digitalRead(GUP);
-  int currentGDownState = !digitalRead(GDOWN);
-
-    if (currentGUpState != lastGUpState)
-    {
-      myJoystick.setButton(0, currentGUpState);
-      lastGUpState = currentGUpState;
-    }
-
-        if (currentGDownState != lastGDownState)
-    {
-      myJoystick.setButton(1, currentGDownState);
-      lastGDownState = currentGDownState;
-    }
-
-  // Check if setup mode is active
   if (setupMode) {
-    // Print raw and processed handbrake value
     Serial.print("Raw Handbrake Value: ");
-    Serial.print(rawHandbrakeOriginal);
+    Serial.print(rawHandbrake);  // Print the original, unprocessed value
     Serial.print("   Processed Handbrake Value: ");
-    Serial.println(rawHandbrake);
+    Serial.println(processedHandbrake);  // Print the processed value
   }
 }
